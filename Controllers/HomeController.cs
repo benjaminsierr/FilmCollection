8f223e563ddfaaa60744721404a08b538f235c21
benjaminsierr <65034700+benjaminsierr@users.noreply.github.com>
2021-01-28 14:41:59 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using FilmCollection.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmCollection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("NewMovie")]
+        public IActionResult NewMovie()
+        {
+            return View();
+        }
+
+        [HttpPost("NewMovie")]
+        public IActionResult NewMovie(Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+                
+            }
+            MovieList.AddMovie(movie);
+            Debug.WriteLine("Title: " + movie.Title);
+            return View("Movies", MovieList.Movies);
+        }
+
+
+
+        public IActionResult Movies()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        
+        public IActionResult Podcast()
+        {
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+//Category
+//Title
+//Year
+//Director
+//Rating
+//Edited
+//Lent To:
+//Notes
+namespace FilmCollection.Models
+{
+    public class Movie
+    {
+        [Required(ErrorMessage = "This field is required")]
+        public string Category { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        public int Year { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        public string Director { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        public string Rating { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        public bool Edited { get; set; }
+        public string LentTo { get; set; }
+        public string Notes { get; set; }
+
+
+    }
+}
